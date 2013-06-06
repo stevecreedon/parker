b0f96c9a02353fb964f370bb588af71d5feea53e
stevecreedon <steve@creedon.me>
2013-06-06 15:07:33 +0100

    added autoscale up and down policies

--- lib/parker/auto_scale/create.rb
+++ lib/parker/auto_scale/create.rb
@@ -11,6 +11,32 @@ recipe = YAML.load_file(Parker::AutoScale::Recipes[index.to_i][:path])
 
 configuration = Parker.connection.auto_scale.configurations.create(recipe[:configuration])
 
-Parker.connection.auto_scale.groups.create(recipe[:group].merge(launch_configuration_name: configuration.id))
+group = Parker.connection.auto_scale.groups.create(recipe[:group].merge(launch_configuration_name: configuration.id))
+
+policy = recipe[:scale_up_policy]
+Parker.connection.auto_scale.put_scaling_policy(policy[:adjustment_type], 
+                                                policy[:group_name], 
+                                                "#{policy[:policy_name]}-#{policy[:adjustment]}", 
+                                                policy[:adjustment], 
+                                                {"Cooldown" => policy[:cooldown]})
+
+scale_up_policy = Parker.connection.auto_scale.policies.get("#{policy[:policy_name]}-#{policy[:adjustment]}")
+
+recipe[:scale_up_alarms].each do |alarm|
+  alarm = Parker.connection.cloudwatch.alarm_data.create(alarm.merge(alarm_actions: [scale_up_policy.arn]))
+end
+
+policy = recipe[:scale_down_policy]
+Parker.connection.auto_scale.put_scaling_policy(policy[:adjustment_type], 
+                                                policy[:group_name], 
+                                                "#{policy[:policy_name]}-#{policy[:adjustment]}", 
+                                                policy[:adjustment], 
+                                                {"Cooldown" => policy[:cooldown]})
+
+scale_down_policy = Parker.connection.auto_scale.policies.get("#{policy[:policy_name]}-#{policy[:adjustment]}")
+
+recipe[:scale_down_alarms].each do |alarm|
+  alarm = Parker.connection.cloudwatch.alarm_data.create(alarm.merge(alarm_actions: [scale_down_policy.arn]))
+end
 
 
