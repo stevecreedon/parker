64e43a30261da4b6a969c35504eeddeb6fe5b27c
Arturs Kreipans <arturs.kreipans@gmail.com>
2013-06-07 15:04:29 +0100

    fixes path to the recepies

--- lib/parker/server/recipes.rb
+++ lib/parker/server/recipes.rb
@@ -1,7 +1,10 @@
 module Parker 
   module Server
     module Recipes  
-    RECIPES =  Dir.glob("recipes/server/*.yaml").collect{|f| {name: f.gsub(".yaml","").gsub("recipes/server/",""), path: f}}
+
+      RECIPES =  Dir.glob(File.expand_path("../../../recipes/server/*.yaml", __FILE__)).collect do | f |
+        { name: f.gsub(".yaml","").gsub("recipes/server/",""), path: f }
+      end
 
       def self.[](i)
         RECIPES[i]
